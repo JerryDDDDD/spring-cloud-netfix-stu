be3b5fc58abc7b6998959934c5a8ea508f1fa12b
yezewen <569193365@qq.com>
2019-08-23 22:29:19 +0800

    config server 端创建

---
+++ config/src/main/java/com/layman/ConfigApplication.java
@@ -0,0 +1,23 @@
+package com.layman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @ClassName ConfigApplication
+ * @Description TODO
+ * @Author 叶泽文
+ * @Data 2019/8/23 22:17
+ * @Version 3.0
+ **/
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
